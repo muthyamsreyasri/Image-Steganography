2fb585b4fd968a3138b8a2e8b84b867814279e7b
muthyamsreyasri <154613752+muthyamsreyasri@users.noreply.github.com>
2024-12-04 16:48:31 +0530

    Add files via upload

---
+++ AppForm.java
@@ -0,0 +1,34 @@
+package com.voidmain.pojo;
+
+public class AppForm {
+
+	private String image;
+	private String key;
+	private String type;
+	private String message;
+	
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
